ea04f93673c7de86052f1c39dbf427e4a152d890
s-chanbr <s-chanbr@53S500036626.it.bsd405.org>
2018-09-18 09:49:20 -0700

    Turning in Proj 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,17 +1,84 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
+public class ASCIIArt 
+{
+	public static void sceneryTop()
+	//Creates the top of trees and mountains
+	{
+		System.out.println("           /                 \\                       /                 \\         ");
+		System.out.println("      /\\  /         /\\        \\   /\\            /\\  /         /\\        \\   /\\");
+		System.out.println("     /  \\/         /  \\        \\ /  \\          /  \\/         /  \\        \\ /  \\");
+		System.out.println("    /    \\        /    \\        /    \\        /    \\        /    \\        /    \\");
+		System.out.println("   /      \\      /      \\      /      \\      /      \\      /      \\      /      \\");
+	}
+	
+	public static void treeTrunks()
+	//Creates the base of the trees
+	{
+		System.out.println("   *--| |--*     *--| |--*     *--| |--*     *--| |--*     *--| |--*     *--| |--*");
+	}
+	public static void road()
+	//Creates a road for cars
+	{
+		System.out.println("________________________________________________________________________________\n\n");
+		System.out.println("---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---");
+		System.out.println("\n________________________________________________________________________________");
+	}
+	public static void houseWallRow1()
+	//Creates the walls for a row of houses
+	{
+		 System.out.println(" |             |     |             |     |             |     |             |   ");
+	}
+	public static void houseWallRow2()
+	//Doubles the lengths of the walls for a row of houses
+	{
+		houseWallRow1();
+		houseWallRow1();
+	}
+	public static void sidewalk()
+	//Creates a sidewalk
+	{
+		System.out.println("---------------------------------------------------------------------------------");
+		System.out.println("    *\"    *\"    *\"    *\"    *\"    *\"    *\"    *\"    *\"    *\"    *\"    *\"    *\"");
+		System.out.println("   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"   *\"");
+		System.out.println("---------------------------------------------------------------------------------");
+	}
 
-// YOUR CODE GOES HERE :)
+	public static void houseRow()
+	//Creates a row of 4 houses
+	{
+		System.out.println("   _II________         _II________         _II________         _II________");
+		System.out.println("  /-----------\\       /-----------\\       /-----------\\       /-----------\\   ");
+		System.out.println(" /-------------\\     /-------------\\     /-------------\\     /-------------\\ ");
+		System.out.println("/_______________\\   /_______________\\   /_______________\\   /_______________\\");
+		houseWallRow2();
+		System.out.println("");
+	}
+	
+	public static void forest()
+	//Creates a forest with mountains in the background
+	{
+		sceneryTop();
+		treeTrunks();
+	}
+	
+	public static void Snowyville()
+	//Creates a small town featuring a forest and 8 houses with side walks
+	{
+		forest();
+		sidewalk();
+		road();
+		houseRow();
+		houseRow();
+		road();
+		sidewalk();
+	}
+
+	public static void main(String[] args) 
+	//Creates the town of Snowyville with a beautiful caption!!
+	{
+	Snowyville();
+	System.out.println("Snowyville, the toun that lies beneath the legendary Arzic Mountains!");
+	System.out.println("You plan on going up there you say? You're quite brave!");
+	System.out.println("It'll be a long journey so make sure you're prepared!");
+	}
+
+}
